fb845325ab600446d2fac93a56624da4e4414c9d
Hemanth Mudra <mudra.hemanth@gmail.com>
2020-07-31 16:54:40 +0530

    header and footer

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
 import Menu from './MenuComponents';
 import { DISHES } from '../shared/dishes';
-import Dishdetail from './DishDetailComponent'
-
+import Dishdetail from './DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
    constructor(props) {
@@ -23,14 +24,11 @@ class Main extends Component {
    render() {
       return (
          <div >
-            <Navbar dark color="primary">
-               <div className="container">
-                  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-               </div>
-            </Navbar>
+           <Header/>
             <Menu dishes={this.state.dishes}
                onClick={(dishId) => this.onDishSelect(dishId)} />
             <Dishdetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+            <Footer/>
          </div>
       );
    }
